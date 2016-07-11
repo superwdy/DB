f40b7c760aea922da1f406fcfdf5fbdafa4a8c12
Henry Chen <henrycychen@gmail.com>
2016-07-11 10:45:28 -0700

    Updated the objectives and outcomes.

--- api_tests/test_files_upload.py
+++ api_tests/test_files_upload.py
@@ -1,67 +1,69 @@
 # Objective - Test if the api can upload a file to DB.
-# Expected Outcome - The file can be seen on DB.
+# Expected Outcome - Assert http status code == 200 and use the search
+# api to see that the file exists.
 def test_upload_validation():
     assert True == False
 
 # Objective - Input an acceptable upload path to upload a file to DB.
-# Expected Outcome - if upload path is acceptable, uploaded file will
-# be on DB.
+# Expected Outcome - Assert that the file was uploaded using the search
+# api.
 def test_upload_path():
     assert True == False
 
 # Objective - Input an unacceptable upload path on DB (use incorrect
 # formats, ie. use '\' instead of '/')
-# Expected Outcome - should receive an error when curling, which should
-# prevent file from uploading.
+# Expected Outcome - Assert that http error code != 200 and response
+# is returned with "path does not match..."
 def test_upload_path_invalid():
     assert True == False
 
 # Objective - Make sure there is an existing file before uploading the
 # same file. Select mode:add when uploading.
-# Expected Outcome - Existing file should not be overwritten, instead
-# the same file name should exist with an appended '#'. Ex:
-# "Document(2).txt"
+# Expected Outcome - Assert http code == 200 and use the search api to
+# find the file name that has been overwritten with "<filename>(2).ext"
 def test_upload_mode_add():
     assert True == False
 
 # Objective - Make sure there is an existing file before uploading the
 # same file. Select mode:upload when uploading.
-# Expected Outcome - File uploaded will overwrite the existing file. Name
-# will be the same.
+# Expected Outcome - Assert http code == 200 and use the search api to
+# confirm there is not a duplicate filename.
 def test_upload_mode_override():
     assert True == False
 
 # Objective - Make sure there is an existing file before uploading the
 # same file. Select mode:update when uploading.
-# Expected Outcome - File will overwrite if the rev is the same. The
-# autorename will add string "conflicted copy" to the file.
+# Expected Outcome - Assert http code == 200 and use the search api to
+# find same file name with appended "conflicted copy" string.
 def test_upload_mode_update():
     assert True == False
 
 # Objective - Test to see what makes the upload mode invalid. Ex: Leave
-# the field blank when uploading (not sure this is right).
-# Expected Outcome - This will default to the "add" option, which will
-# not overwrite the file, but will create another file and append a #
-# to it.
+# the field blank when uploading.
+# Expected Outcome - Assert using the search api and find the uploaded
+# file with appended "(2).ext" after the file name.
 def test_upload_mode_invalid():
     assert True == False
 
 # Objective - Test to see if DB will autorename the file if there is a
 # conflict with the mode.
-# Expected Outcome - File will be autorenamed by DB.
+# Expected Outcome - Assert http code == 200 and use the search api to
+# find the same file (DB will not autorename the file).
 def test_upload_autorename_false()
     assert True == False
 
 # Objective - Test to see if DB will autorename the file if there is a
 # conflict with the mode.
-# Expected Outcome - File will be autorenamed by DB.
+# Expected Outcome - Assert http code == 200 and use the search api to
+# find the amended file name (DB will autorename the file).
 def test_upload_authorname_true():
     assert True == False
 
 # Objective - See if inputting a timestamp when a user uploads the file
 # will appear on the "client modified" response.
-# Expected Outcome - Time stamp appears exactly what was inputted by the
-# user under "client modified" response.
+# Expected Outcome - Assert http code == 200 and use the search api and
+# assert the "client modified" field of the metadata is the same as
+# what was inputted
 def test_upload_client_modified_future():
     assert True == False
 
@@ -81,39 +83,41 @@ def test_upload_mute_false():
 
 # Objective - Test to see if original uploaded file size is the same as
 # the size response.
-# Expected Outcome - Size reponse and original uploaded file size are
-# equal.
+# Expected Outcome - Assert using the search api and confirm that the
+# size field == what was uploaded.
 def test_upload_response_size():
     assert True == False
 
-# Objective - Check to see if the response for pathlower starts with a '/'
-# Expected Outcome - Response field pathlower will start with '/'.
+# Objective - Upload a file and make sure the file path is correct.
+# Expected Outcome - Assert http response == 200 and use the search
+# api and check to see if "path_lower" field starts with a '/'
 def test_upload_response_pathlower():
     assert True == False
 
 # Objective - Create an error that returns a malformed response.
-# Expected Outcome - Receive a malformed response error message that
-# could not save the file.
+# Expected Outcome - Assert http code != 200 and will receive a
+# malformed response error message stating that the file could not be
+# saved.
 def test_upload_error_malformed_path():
     assert True == False
 
 # Objective - Create a situation that does not give the user permission
 # to write to the target location.
-# Expected Outcome - User receives an error and cannot write the file to
-# the target location.
+# Expected Outcome - Assert http code != 200 and use the search api to
+# assert the file does not exist.
 def test_upload_error_no_write_permission():
     assert True == False
 
 # Objective - Create a situation where the user goes over the available
 # space (bytes) when uploading a file.
-# Expected Outcome - User received an error that they cannot write the
-# file to target location due to insufficient space
+# Expected Outcome - Assert http code != 200 and use the search api to
+# assert the file does not exist.
 def test_upload_error_insufficient_space():
     assert True == False
 
 # Objective - Upload a file or folder with an inappropriate name or name
 # format.
-# Expected Outcome - Receive an error that the file cannot be saved due
-# to the file or folder name.
+# Expected Outcome - Assert http code != 200 and use the search api to
+# assert the file does not exist.
 def test_upload_error_disallowed_name():
     assert True == False
