0b86a3a01e6f89c9007c941d7e28e4f6f481998f
Henry Chen <henrycychen@gmail.com>
2016-07-11 22:44:22 -0700

    Added a couple of classes

--- helper_library_DB.py
+++ helper_library_DB.py
@@ -1,16 +1,34 @@
+"""
+This is a helper library that includes a couple of classes to assist in running tests for the API
+"""
 
-"""
-Code	Description
-400	Bad input parameter. Error message should indicate which one and why.
-401	Bad or expired token. This can happen if the user or Dropbox revoked or expired an access token. To fix, you should re-authenticate the user.
-403	Bad OAuth request (wrong consumer key, bad nonce, expired timestamp...). Unfortunately, re-authenticating the user won't help here.
-404	File or folder not found at the specified path.
-405	Request method not expected (generally should be GET or POST).
-429	Your app is making too many requests and is being rate limited. 429s can trigger on a per-app or per-user basis.
-503	If the response includes the Retry-After header, this means your OAuth 1.0 app is being rate limited. Otherwise, this indicates a transient server error, and your app should retry its request.
-507	User is over Dropbox storage quota.
-5xx	Server error. Check DropboxOps.
-"""
-hostname = ['api.dropboxapi.com', 'content.dropboxapi.com','notify.dropboxapi.com']
-response_type = ''
-client_id = ''
+import json
+import requests
+from faker import Factory
+faker = Factory.create()
+
+class Dropbox(object):
+
+    db_upload_url = "https://content.dropboxapi.com/2/files/upload"
+    db_search_url = "https://api.dropboxapi.com/2/files/search"
+
+    def db_upload(self, my_headers, my_data):
+        return requests.post(Dropbox().db_upload_url, headers=my_headers, data=my_data)
+
+    def db_search(self, my_data2):
+        my_headers2 = {
+        "Authorization": "Bearer Wvf7h-SwacMAAAAAAAAEDlwLfld0CIMHySGJZuWrYWwQo-lPu8ukW3Qbd3vsx2C8",
+        "Content-Type": "application/json"
+        }
+        return requests.post(Dropbox().db_search_url, headers=my_headers2, data=json.dumps(my_data2))
+
+class Fake(object):
+
+    def create_fake_name(self):
+        return faker.name()
+
+    def create_first_name(self):
+        return faker.first_name()
+
+    def create_last_name(self):
+        return faker.last_name()
