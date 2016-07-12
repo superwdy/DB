7356d415fe6b82f437399255761c9e394288465d
Henry Chen <henrycychen@gmail.com>
2016-07-12 13:20:47 -0700

    Added a create file function

--- helper_library_DB.py
+++ helper_library_DB.py
@@ -1,14 +1,22 @@
 """
-This is a helper library that includes a couple of classes to assist in running tests for the API
+This is a helper library that includes a couple of class objects for
+testing dropbox APIs.
+
+class Dropbox stores all request information to POST/GET/DELETE
+information to and from their database.
+
+class Fake creates fake data and files for testing purposes.
 """
 
 import json
 import requests
+import random
 from faker import Factory
 faker = Factory.create()
 
 class Dropbox(object):
 
+    authorization = "Bearer Wvf7h-SwacMAAAAAAAAECLY51n13M2ltZImFjM9M242G96wJV32mWiXR8F9fwVvl"
     db_upload_url = "https://content.dropboxapi.com/2/files/upload"
     db_search_url = "https://api.dropboxapi.com/2/files/search"
 
@@ -17,7 +25,7 @@ class Dropbox(object):
 
     def db_search(self, my_data2):
         my_headers2 = {
-        "Authorization": "Bearer Wvf7h-SwacMAAAAAAAAEDlwLfld0CIMHySGJZuWrYWwQo-lPu8ukW3Qbd3vsx2C8",
+        "Authorization": Dropbox().authorization,
         "Content-Type": "application/json"
         }
         return requests.post(Dropbox().db_search_url, headers=my_headers2, data=json.dumps(my_data2))
@@ -32,3 +40,14 @@ class Fake(object):
 
     def create_last_name(self):
         return faker.last_name()
+
+    def create_file_name(self):
+        ext_list = ['.txt', '.mpg', '.jpg']
+        chosen_ext = ext_list[random.randint(0,2)]
+        return faker.first_name() + chosen_ext
+
+    def create_file(self,fname=''):
+        filename = fname
+        new_file = open(fname, 'a+')
+        new_file.truncate(random.randint(0,1024*1024))
+
