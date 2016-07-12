0c5ae18ab870846e97dfc4e4a854876a69b3d0de
Henry Chen <henrycychen@gmail.com>
2016-07-11 23:08:09 -0700

    Finished one test in a few hours. I'm so fast!

--- api_tests/test_files_upload.py
+++ api_tests/test_files_upload.py
@@ -1,9 +1,47 @@
+"""
+This is a Dropbox API. All information can be found at the below web address:
+https://www.dropbox.com/developers/documentation/http/documentation.
+"""
+import json
+import pytest
+import requests
+import time
+import os
+from helper_library_DB import Dropbox
+from helper_library_DB import Fake
+
+d = Dropbox()
+f = Fake()
+
 # Objective - Test if the api can upload a file to DB.
 # Expected Outcome - Assert http status code == 200 and use the search
 # api to see that the file exists.
 def test_upload_validation():
-    assert True == False
 
+    #Creating a fake directory name (First 4 lines)
+    fake_name = f.create_first_name()
+    base_dir = "{\"path\":\"/"
+    filename = fake_name+".jpg"+"\"}"
+    db_path = os.path.join(base_dir, filename)
+
+    my_headers = {
+        "Authorization": d.authorization,
+        "Content-Type": "application/octet-stream",
+        "Dropbox-API-Arg": db_path
+    }
+    my_data = open("C:/Users/Henry/Pictures/IMG_0399.JPG", "rb").read()
+    r1 = d.db_upload(my_headers=my_headers,my_data=my_data)
+
+    #Have to set a delay, otherwise the assert will check before the file has
+    # been uploaded into the DB database.
+    time.sleep(10)
+
+    my_data2 = {"path": "", "query": fake_name+'.jpg'}
+    r2 = d.db_search(my_data2=my_data2)
+    check_assert = json.loads(r2.text)['matches'][0]['metadata']['name']
+    assert r1.status_code == 200 and check_assert == fake_name+'.jpg'
+
+"""
 # Objective - Input an acceptable upload path to upload a file to DB.
 # Expected Outcome - Assert that the file was uploaded using the search
 # api.
@@ -121,3 +159,5 @@ def test_upload_error_insufficient_space():
 # assert the file does not exist.
 def test_upload_error_disallowed_name():
     assert True == False
+
+"""
